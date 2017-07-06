c9725c1ad63e29a66c13ea589c7e34d1fc38b1ee
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2017-07-06 08:17:39 +0100

    Fix missing last column
    It seems I forgot to commit this file. Oops!

---
+++ src/CRenderer.cpp
@@ -0,0 +1,95 @@
+#include <array>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <map>
+
+#include <cstdlib>
+#include <cstdio>
+#include <functional>
+#include <cmath>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cstring>
+#include <memory>
+#include <iostream>
+#include <map>
+#include <array>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+#include "Common.h"
+#include "CGameSnapshot.h"
+#include "CGame.h"
+#include "CRenderer.h"
+#include "NativeBitmap.h"
+#include "LoadPNG.h"
+
+namespace odb {
+
+    auto texture = loadPNG("res/tile0.png");
+
+    void CRenderer::setSnapshot( const CGameSnapshot& snapshot ) {
+        this->mGameSnapshot = snapshot;
+    }
+
+
+    void CRenderer::render( long ms ) {
+
+
+      //TODO: move into  constant
+      const static Vec2f blockSize = { 32, 32 };
+
+      //TODO: move into  constant
+      const static Vec2f mapSize = { 40, 40 };
+
+      fill( 0, 0, xRes, yRes, {0, 96, 96, 96} );
+      fill( 0, yRes / 2, xRes, yRes / 2, {0, 192, 192, 192} );
+
+
+      constexpr auto columnsPerDegree = (xRes / 90)+1;
+      auto column = 0;
+
+      for (int d = -45; d <= 45; ++d) {
+          auto rayCollision = mGameSnapshot.mCurrentScan[ d + 45 ];
+          float ray = rayCollision.mCachedDistance;
+          int distance = (yRes / ray);
+          float hueX =  rayCollision.mCollisionPoint.mX;
+          float hueZ = rayCollision.mCollisionPoint.mY;
+
+          int cellX = hueX;
+          int cellZ = hueZ;
+
+          int dx = ( hueX - cellX ) * blockSize.mX;
+          int dz = ( hueZ - cellZ ) * blockSize.mY;
+
+          int columnHeight = distance;
+
+          for ( int y = 0; y <= columnHeight; ++y ) {
+
+              int v = ( texture->getHeight() * y) / columnHeight;
+              int ux = (texture->getWidth() * dx) / mapSize.mX;
+              int uz = (texture->getWidth() * dz) / mapSize.mY;
+              unsigned int pixel = texture->getPixelData()[ ( texture->getWidth() * v ) + ((ux + uz ) % texture->getWidth()) ];
+
+              fill( column,
+                    (yRes / 2 - (distance / 2) + y ),
+                    (columnsPerDegree),
+                    1,
+                    {0 , (pixel & 0xFF) >> 0, (pixel & 0x00FF00) >> 8, (pixel & 0xFF0000) >> 16 }
+              );
+          }
+
+          column += columnsPerDegree;
+      }
+
+
+
+      flip();
+
+  }
+}
